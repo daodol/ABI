f8d56293c0834c7c738bfd5c90b5928317e68539
fengdaoqiang <1530566718@qq.com>
2018-05-23 21:45:03 +0800

    添加.net调用cplus

---
+++ DotNetCplus/DotNetCall/NativeHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Helper
+{
+    public class NativeHelper
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string path);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool FreeLibrary(IntPtr module);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr lib,string funcName);
+
+        [DllImport("WS2_32.DLL")]
+        public static extern int WSAStartup(int wVersionRequested, IntPtr lpWsaData);
+
+        [DllImport("WS2_32.DLL")]
+        public static extern int WSACleanup();
+
+        [DllImport("kernel32.dll",CharSet =CharSet.Ansi)]
+        public static extern void RtlMoveMemory(ref IntPtr dst, IntPtr src, int size);
+
+        [DllImport("kernel32.dll")]
+        public static extern void CopyMemory(ref IntPtr dst, IntPtr src, int size);
+
+        public static int MakeLong(int lowPart, int highPart)
+        {
+            return (int)(((ushort)lowPart) | (uint)(highPart << 16));
+        }
+
+        [DllImport("kernel32")]
+        private static extern bool HeapFree(IntPtr hHeep, uint dwFlags, IntPtr lpMem);
+
+
+        [DllImport("kernel32")]
+        private static extern bool HeapAlloc(IntPtr hHeep, uint dwFlags, int lpMem);
+
+        [DllImport("kernel32")]
+        private static extern IntPtr GetProcessHeap();
+
+        internal static void HEAP_FREE(IntPtr lpMem)
+        {
+            IntPtr pHeap = GetProcessHeap();
+            HeapFree(pHeap, 0, lpMem);
+        }
+
+        internal static void HEAP_ALLOC(int size)
+        {
+            IntPtr pHeap = GetProcessHeap();
+            HeapAlloc(pHeap, 0, size);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    internal struct WsaData
+    {
+        public readonly ushort wVersion;
+        public readonly ushort wHighVersion;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256 + 1)]
+        public readonly string szDescription;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128 + 1)]
+        public readonly string szSystemStatus;
+
+        public readonly ushort iMaxSockets;
+        public readonly ushort iMaxUdpDg;
+        public readonly IntPtr lpVendorInfo;
+        //char FAR* lpVendorInfo;
+    }
+}
---
+++ DotNetCplus/DotNetCall/Program.cs
@@ -0,0 +1,34 @@
+﻿using Helper;
+using System;
+using System.Runtime.InteropServices;
+
+namespace DotNetCall
+{
+    [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public delegate void helloDelegate(IntPtr ths,string str);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate  void  CreateDelegate(ref IntPtr ptr);
+
+    struct MyClass
+    {
+        public IntPtr hello;
+    }
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+           var dllHandle= NativeHelper.LoadLibrary("DotNetCalled.dll");
+           var createAddr = NativeHelper.GetProcAddress(dllHandle, "Create");
+           var create=Marshal.GetDelegateForFunctionPointer<CreateDelegate>(createAddr);
+             
+           var _objptr=IntPtr.Zero;
+           create(ref _objptr);
+           IntPtr dst =IntPtr.Zero;
+           NativeHelper.RtlMoveMemory(ref dst, _objptr, IntPtr.Size);
+           var myclass= Marshal.PtrToStructure<MyClass>(dst);
+           var func = Marshal.GetDelegateForFunctionPointer<helloDelegate>(myclass.hello);
+           func(_objptr, "helloworld");
+        }
+    }
+}
